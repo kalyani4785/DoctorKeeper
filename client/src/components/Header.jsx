562aba313b58fe0218d5f7f1bfddd0d1d3b39a66
ASHISH KUMAR YADAV <kalyanidubey001@gmail.com>
2020-12-22 16:05:22 +0530

    deploy2

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
        </div>
        <div class="col-6 " >
   <a id="header-button" style={{position:"right"}}  class="btn" style={{bottom:"5%" ,right:"2%"}} href="https://github.com/ASHISH-GITHUB2495/visitfirst"  target="_blank" >See on Github</a>
-  <a id="header-button" style={{position:"right"}} class="btn" style={{bottom:"5%" ,left:"2%"}} href="https://ashish-github2495.github.io/MyPortfolio.github.io/"  target="_blank" >About Developer</a>
+  <a id="header-button" style={{position:"right"}} class="btn" style={{bottom:"5%" ,left:"2%"}} href="https://docs.google.com/document/d/1By6kaPKucAwuhO6IDKmxdStg59wExpWNtxeqZo4cg34/edit?usp=sharing"  target="_blank" >About Developer</a>
   </div>
      </div>
      
